f0f27384c45c41ed78a0fdfba9215326d0f416e8
Mousmi Sorathiya <sora0008@algonquinlive.com>
2023-11-22 22:55:38 -0500

    geometry

--- MousmiS_BrigitteH/geometry.py
+++ MousmiS_BrigitteH/geometry.py
@@ -11,10 +11,13 @@ class Line:
     def __init__(self,from_point, to_point):
         self.__from_point = from_point
         self.__to_point = to_point
+        self.p = None
+        from_point = Point()
+        to_point = Point()
+        self.__length = (((to_point.__x - from_point.__x)**2 + (to_point.__y - from_point.__y)**2)**0.5)
     
     @property
     def length(self):
-        from_point = Point()
-        to_point = Point()
-        self.__length = ((to_point.__x - from_point.__x)**2 + (to_point.__y - from_point.__y)**2)**0.5
         return self.__length
+    
+p = Point()    
